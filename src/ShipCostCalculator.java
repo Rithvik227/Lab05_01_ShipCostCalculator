2dd36121f0e7982d67973f209e7555d987402ef8
Rithvik227 <rithvik2009bg@gmail.com>
2026-06-14 21:32:36 -0400

    Finished Task 1

---
+++ src/ShipCostCalculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class ShipCostCalculator {
+    static void main(String[] args) {
+        //get user input
+        //initialize itemPrice and shippingCost variables as floats
+        //check if input is a float
+        //check if item price is less than 100
+        //calculate shipping cost
+        //print shipping cost and total cost
+        //print invalid input message
+
+        Scanner in = new Scanner(System.in);
+        float itemPrice = 0;
+        float shippingCost = 0;
+        System.out.print("Enter your item's price: ");
+
+        if (in.hasNextFloat()) {
+            itemPrice = in.nextFloat();
+            in.nextLine();
+            if (itemPrice < 100) {
+                shippingCost = itemPrice * 0.02f;
+            } else {
+                shippingCost = 0;
+            }
+
+            System.out.println("\nThe shipping cost for your item is: $" + shippingCost);
+            System.out.println("\nThe total cost for your item is: $" + (itemPrice + shippingCost));
+
+        } else {
+            String trash = in.nextLine();
+            System.out.println("\nYou entered: " + trash);
+            System.out.println("\nThis input is invalid");
+        }
+    }
+}
